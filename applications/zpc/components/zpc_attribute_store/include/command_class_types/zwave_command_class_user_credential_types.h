a3c8925aa5e49ce6245f830dca297dfac532860f
Boris Labbé <boris.labbe@silabs.com>
2024-04-16 16:59:14 +0200

    GH-43: ZPC User Credential Discovery Commands
    - User Capabilities
    - Credential Capabilities
    - All User Checksum
    - User Get/Report
    - Credential Get/Report
    
    Merged :
    UIC-3222: Fix Supported Credential Types
    UIC-3222: Ignore User ID 0 & don't create duplicates
    UIC-3222: Delayed interview
    UIC-3222: User Credential User Name Encoding under UUID (No real advantage to put this under USER_NAME)
    UIC-3222: Fix user discovery
    UIC-3222: Credential get log
    UIC-3222: Discovery fix (credential and user get)
    UIC-3222: Discovery interview remove all users before user interview
    UIC-3222: Fix credential report if credential data is missing
    UIC-3222: Remove user if User modifier type is DNE
    UIC-3222: Correctly remove all user credential before discovery
    UIC-3222: set_reported_attribute return correct status and undefined desired
    UIC-3222: Add get_desired user id node function
    UIC-3222: get_credential_type can now be searched as desired value
    
    Bug-SiliconLabs: UIC-3222
    Bug-GitHub: https://github.com/SiliconLabs/UnifySDK/pull/43
    Signed-off-by: Philippe Coval <philippe.coval@silabs.com>

---
+++ applications/zpc/components/zpc_attribute_store/include/command_class_types/zwave_command_class_user_credential_types.h
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * # License
+ * <b>Copyright 2023 Silicon Laboratories Inc. www.silabs.com</b>
+ ******************************************************************************
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ *****************************************************************************/
+
+/**
+ * @defgroup zwave_command_class_user_credential_types Type definitions for attribute storage of the Sound Switch Command Class
+ * @ingroup zpc_attribute_store_command_classes_types
+ * @brief Type definitions for the Sound Switch Command Class.
+ *
+ * @{
+ */
+
+#ifndef ZWAVE_COMMAND_CLASS_USER_CREDENTIALS_TYPES_H
+#define ZWAVE_COMMAND_CLASS_USER_CREDENTIALS_TYPES_H
+
+#include <stdint.h>
+
+///> Supported credential rules bitmask. uint8_t
+typedef uint8_t user_credential_supported_credential_rules_t;
+
+///> Supported user type bitmask. uint32_t
+typedef uint32_t user_credential_supported_user_type_bitmask_t;
+
+///> All user checksum. uint16_t
+typedef uint16_t user_credential_all_users_checksum_t;
+
+///> User Unique ID. uint16_t
+typedef uint16_t user_credential_user_unique_id_t;
+///> User Modifier Type. uint8_t
+typedef uint8_t user_credential_user_modifier_type_t;
+///> User Modifier Node ID. uint16_t
+typedef uint16_t user_credential_user_modifier_node_id_t;
+///> User Type. uint8_t
+typedef uint8_t user_credential_user_type_t;
+
+///> Credential type. uint8_t
+typedef uint8_t user_credential_type_t;
+
+///> Credential slot. uint16_t
+typedef uint16_t user_credential_slot_t;
+
+///> Operation type. uint8_t
+typedef uint8_t user_credential_operation_type_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  //ZWAVE_COMMAND_CLASS_USER_CREDENTIALS_TYPES_H
+/** @} end zwave_command_class_user_credentials_types */
